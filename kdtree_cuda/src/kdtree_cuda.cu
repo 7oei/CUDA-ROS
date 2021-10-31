be35e0881d3c0413030e28e4ee66123ae7d23286
7oei <adachi-n@roboken.iit.tsukuba.ac.jp>
2021-10-31 20:15:43 +0900

    host kd

--- kdtree_cuda/src/kdtree_cuda.cu
+++ kdtree_cuda/src/kdtree_cuda.cu
@@ -28,10 +28,6 @@ typedef struct
 	bool node_is_right;
 } node;
 
-std::vector <node> nodes;
-
-int root_id=-1;
-
 bool first=true;
 
 //	年齢(昇順)
@@ -51,7 +47,8 @@ int AxisSort(const void * n1, const void * n2)
 	}
 }
 
-int CreateTree(std::vector<std::vector<float>> points,std::vector<int> group_indices,int parent_id,bool node_is_right){
+int CreateTree(int* root_id,std::vector <node>& nodes, std::vector<std::vector<float>> points,std::vector<int> group_indices,int parent_id,bool node_is_right)
+{
 	//入力データ初期化
 	int group_size = group_indices.size();
 	// std::cout<<"group_size"<<group_size<<std::endl;
@@ -82,7 +79,7 @@ int CreateTree(std::vector<std::vector<float>> points,std::vector<int> group_ind
 			axis_point_with_ids[sort_axis][i].pos[2] = point_with_ids[i].pos[2];
 			// printf("%d, %f, %f, %f \n", point_with_ids[i].id, point_with_ids[i].pos[0], point_with_ids[i].pos[1], point_with_ids[i].pos[2]);
 		}
-		std::cout<<std::endl;
+		// std::cout<<std::endl;
 		//max,min,median,axis_median_id取得
 		max[sort_axis]=point_with_ids[group_size-1].pos[sort_axis];//minとmaxいらんかも
 		min[sort_axis]=point_with_ids[0].pos[sort_axis];
@@ -93,7 +90,7 @@ int CreateTree(std::vector<std::vector<float>> points,std::vector<int> group_ind
 	// std::cout<<"x_length = "<< length[0] <<", x_median["<<axis_median_id[0]<<"] = "<<median[0]<<std::endl;
 	// std::cout<<"y_length = "<< length[1] <<", y_median["<<axis_median_id[1]<<"] = "<<median[1]<<std::endl;
 	// std::cout<<"z_length = "<< length[2] <<", z_median["<<axis_median_id[2]<<"] = "<<median[2]<<std::endl;
-	std::cout<<std::endl;
+	// std::cout<<std::endl;
 	// for(int i=0;i<group_size;i++){
 	// 	std::cout<<"point_id["<<i<<"] = "<<point_with_ids[i].id<<std::endl;
 	// }
@@ -124,7 +121,7 @@ int CreateTree(std::vector<std::vector<float>> points,std::vector<int> group_ind
 		if(node_is_right) nodes[parent_id].right_id=median_id;
 	}
 	else{//親なし
-		root_id=median_id;
+		*root_id=median_id;
 	}
 	std::vector<int> right_group(group_size);
 	std::vector<int> left_group(group_size);
@@ -165,17 +162,133 @@ int CreateTree(std::vector<std::vector<float>> points,std::vector<int> group_ind
 	bool right=false;
 	if(group_size>1){//子がいる
 		if(left_group.size()>0){//左に子がいる
-			left= CreateTree(points,left_group,median_id,false);
+			left= CreateTree(root_id,nodes,points,left_group,median_id,false);
 		}
 		else left=true;
 		if(right_group.size()>0){//右に子がいる
-			right= CreateTree(points,right_group,median_id,true);
+			right= CreateTree(root_id,nodes,points,right_group,median_id,true);
 		}
 		else right=true;
 		if(right&&left) return 1;
 	}
 	else return 1;//子がいない
 }
+
+void PointRangeCheckAndAdd(std::vector<int>& range_indices,int head_id,std::vector<std::vector<float>> points,std::vector<float> search_point,float range_sq)
+{
+	float dist_sq = pow(points[head_id][0]-search_point[0],2)+pow(points[head_id][1]-search_point[1],2)+pow(points[head_id][2]-search_point[2],2);
+	if(dist_sq<range_sq){
+		range_indices.push_back(head_id);
+	} 
+}
+
+int SearchSubTree(std::vector<int>& range_indices,int root_id,std::vector <node> nodes,std::vector<std::vector<float>> points,std::vector<float> search_point,float range_sq)
+{
+	int head_id = root_id;
+	bool cross,next_is_right;
+	//潜り
+	while(1){
+		if(search_point[nodes[head_id].axis]>points[head_id][nodes[head_id].axis]) next_is_right = true;
+		else next_is_right = false;
+
+		if(nodes[head_id].right_id>=0&&nodes[head_id].left_id<0) head_id = nodes[head_id].right_id;//一人っ子
+		else if(nodes[head_id].left_id>=0&&nodes[head_id].right_id<0) head_id = nodes[head_id].left_id;//一人っ子
+		else if(nodes[head_id].left_id>=0&&nodes[head_id].right_id>=0){//双子
+			if(next_is_right){//right
+				head_id = nodes[head_id].right_id;
+			}
+			else{//left
+				head_id = nodes[head_id].left_id;
+			}
+		}
+		else break;
+	}
+	//rootが底
+	if(head_id==root_id) {
+		PointRangeCheckAndAdd(range_indices,head_id,points,search_point,range_sq);
+		return 1;
+	}
+	//昇り
+	int last_id;
+	while(1){
+		cross = false;
+		PointRangeCheckAndAdd(range_indices,head_id,points,search_point,range_sq);
+		//昇る
+		last_id = head_id;
+		head_id = nodes[head_id].parent_id;
+		//交差判定　1軸のみ低効率
+		float axis_diff_sq = pow(points[head_id][nodes[head_id].axis] - search_point[nodes[head_id].axis],2);
+		if(axis_diff_sq < range_sq) cross = true;
+		int sub_tree=0;
+		if(cross){
+			if(last_id==nodes[head_id].right_id&&nodes[head_id].left_id>0) sub_tree = SearchSubTree(range_indices,nodes[head_id].left_id,nodes,points,search_point,range_sq);//右から上がってきた
+			if(last_id==nodes[head_id].left_id&&nodes[head_id].right_id>0) sub_tree = SearchSubTree(range_indices,nodes[head_id].right_id,nodes,points,search_point,range_sq);//左から上がってきた
+		}
+		if(head_id==root_id){
+			PointRangeCheckAndAdd(range_indices,head_id,points,search_point,range_sq);
+			break;
+		}
+	}
+	return 1;
+}
+
+// __device__ void d_PointRangeCheckAndAdd(std::vector<int>& range_indices,int head_id,std::vector<std::vector<float>> points,std::vector<float> search_point,float range_sq)//vecotrなしpowなしpushbackなしで作り直す
+// {
+// 	float dist_sq = pow(points[head_id][0]-search_point[0],2)+pow(points[head_id][1]-search_point[1],2)+pow(points[head_id][2]-search_point[2],2);
+// 	if(dist_sq<range_sq){
+// 		range_indices.push_back(head_id);
+// 	} 
+// }
+
+// __device__ int d_SearchSubTree(std::vector<int>& range_indices,int root_id,std::vector <node> nodes,std::vector<std::vector<float>> points,std::vector<float> search_point,float range_sq)//vecotrなしpowなしで作り直す
+// {
+// 	int head_id = root_id;
+// 	bool cross,next_is_right;
+// 	//潜り
+// 	while(1){
+// 		if(search_point[nodes[head_id].axis]>points[head_id][nodes[head_id].axis]) next_is_right = true;
+// 		else next_is_right = false;
+
+// 		if(nodes[head_id].right_id>=0&&nodes[head_id].left_id<0) head_id = nodes[head_id].right_id;//一人っ子
+// 		else if(nodes[head_id].left_id>=0&&nodes[head_id].right_id<0) head_id = nodes[head_id].left_id;//一人っ子
+// 		else if(nodes[head_id].left_id>=0&&nodes[head_id].right_id>=0){//双子
+// 			if(next_is_right){//right
+// 				head_id = nodes[head_id].right_id;
+// 			}
+// 			else{//left
+// 				head_id = nodes[head_id].left_id;
+// 			}
+// 		}
+// 		else break;
+// 	}
+// 	//rootが底
+// 	if(head_id==root_id) {
+// 		d_PointRangeCheckAndAdd(range_indices,head_id,points,search_point,range_sq);
+// 		return 1;
+// 	}
+// 	//昇り
+// 	int last_id;
+// 	while(1){
+// 		cross = false;
+// 		d_PointRangeCheckAndAdd(range_indices,head_id,points,search_point,range_sq);
+// 		//昇る
+// 		last_id = head_id;
+// 		head_id = nodes[head_id].parent_id;
+// 		//交差判定　1軸のみ低効率
+// 		float axis_diff_sq = pow(points[head_id][nodes[head_id].axis] - search_point[nodes[head_id].axis],2);
+// 		if(axis_diff_sq < range_sq) cross = true;
+// 		int sub_tree=0;
+// 		if(cross){
+// 			if(last_id==nodes[head_id].right_id&&nodes[head_id].left_id>0) sub_tree = d_SearchSubTree(range_indices,nodes[head_id].left_id,nodes,points,search_point,range_sq);//右から上がってきた
+// 			if(last_id==nodes[head_id].left_id&&nodes[head_id].right_id>0) sub_tree = d_SearchSubTree(range_indices,nodes[head_id].right_id,nodes,points,search_point,range_sq);//左から上がってきた
+// 		}
+// 		if(head_id==root_id){
+// 			d_PointRangeCheckAndAdd(range_indices,head_id,points,search_point,range_sq);
+// 			break;
+// 		}
+// 	}
+// 	return 1;
+// }
 
 __device__ int EigenJacobiMethod(float *a, float *v, int n, float eps = 1e-8, int iter_max = 100)
 {
@@ -278,14 +391,30 @@ __device__ int EigenJacobiMethod(float *a, float *v, int n, float eps = 1e-8, in
     return cnt;
 } 
 
-__global__ void NormalsGPU(float* points,int point_size,int* neighbor_points_indices,int* neighbor_start_indices,int neighbor_points_count,float* normals,float* curvatures,long long int* covariance_time,long long int* eigen_time) {
+__global__ void NormalsGPU(float* test_points,int* d_parent_ids,int* d_left_ids,int* d_right_ids,int* d_axes,int root_id,int test_points_size,float* points,int point_size,int* neighbor_points_indices,int* neighbor_start_indices,int neighbor_points_count,float* normals,float* curvatures,long long int* covariance_time,long long int* eigen_time)
+{
     // printf("normalsGPU");
     //インデックス取得
     unsigned int ix = threadIdx.x + blockIdx.x * blockDim.x;
     unsigned int idx = ix;
     unsigned int output_id=50;
     // printf("idx = %d ,", idx);
-
+	if(idx==output_id){
+		// std::vector <node> nodes(test_points_size);
+		// std::vector<float> search_point={11,5,0};
+		// std::vector<int> range_indices;
+		// float range_sq = 3*3;
+		// //探索関数の実行
+		// int range_search = d_SearchSubTree(range_indices,root_id,*nodes,*test_points,search_point,range_sq);//nodes,test_points
+		// // std::cout<<"range_indices.size()"<<range_indices.size()<<std::endl;
+		// if(range_search==1) {
+		// 	printf("device range_indices is [");
+		// 	for(int i=0;i<range_indices.size();i++){
+		// 		printf("%d,",range_indices[i]);
+		// 	}
+		// 	printf("]\n");
+		// }
+	}
 
     if(idx<point_size-1){//対象スレッド内のみ計算
         //デバッグ用
@@ -421,27 +550,61 @@ __global__ void NormalsGPU(float* points,int point_size,int* neighbor_points_ind
 
 extern void ComputeNormals(std::vector<std::vector<float>> points_array,std::vector<std::vector<int>> neighbor_points_indices,std::vector<int> neighbor_start_indices,int neighbor_points_count,std::vector<std::vector<float>>& normals_array,std::vector<float>& curvatures_array,std::vector<long long int>& covariance_compute_time,std::vector<long long int>& eigen_compute_time){
 	std::vector<std::vector<float>> points = {
-		{6,1,4},
-		{8,3,6},
-		{11,2,8},
-		{4,3,8},
+		{1,7,0},
+		{2,5,0},
+		{5,2,0},
+		{9,9,0},
+		{11,5,0},
+		{16,12,0},
+		{17,2,0},
+		{17,9,0},
+		{19,12,0},
 	};
-	std::vector<int> root_indices={0,1,2,3};
+	std::vector<int> root_indices={0,1,2,3,4,5,6,7,8};
+
+
+	//nodesとroot_id初期化しなくていい？
+	std::vector <node> nodes;
 	nodes.resize(points.size());
 	// std::cout<<"sizeof(root_indices) = "<<sizeof(root_indices)<<std::endl;
-	
+	int root_id=-1;
+	int create_end = CreateTree(&root_id,nodes,points,root_indices,-1,false);
+	// if(first){
+	// 	if(create_end==1){
+	// 		for(int i=0;i<points.size();i++){
+	// 			std::cout<<"node["<<i<<"] axis = "<<nodes[i].axis<<", parent_id = "<<nodes[i].parent_id<<", left_id = "<<nodes[i].left_id<<", right_id = "<<nodes[i].right_id<<std::endl;
+	// 		}
+	// 	}
+	// } 
+
+	// std::vector<float> search_point={8,1,0};
+	std::vector<float> search_point={11,5,0};
+	std::vector<int> range_indices;
+	float range_sq = 3*3;
+	//探索関数の実行
+	int range_search = SearchSubTree(range_indices,root_id,nodes,points,search_point,range_sq);
+	// std::cout<<"range_indices.size()"<<range_indices.size()<<std::endl;
 	if(first){
-		int create_end=CreateTree(points,root_indices,-1,false);
-		if(create_end==1){
-			for(int i=0;i<points.size();i++){
-				std::cout<<"node["<<i<<"] axis = "<<nodes[i].axis<<", parent_id = "<<nodes[i].parent_id<<", left_id = "<<nodes[i].left_id<<", right_id = "<<nodes[i].right_id<<std::endl;
+		if(range_search==1) {
+			std::cout<<"host range_indices is [";
+			for(int i=0;i<range_indices.size();i++){
+				std::cout<<range_indices[i]<<",";
 			}
+			std::cout<<"]"<<std::endl;
 		}
-		first=false;
-	} 
-	
+	}
+
+
 	// std::cout<<"3.01"<<std::endl;
     //ホスト1次配列宣言
+	//kd
+	std::vector<float> h_test_points(points.size() * 3);
+	std::vector<int> h_parent_ids(points.size());
+	std::vector<int> h_left_ids(points.size());
+	std::vector<int> h_right_ids(points.size());
+	std::vector<int> h_axes(points.size());
+
+	//normal
     std::vector<float> h_points(points_array.size() * 3);
     std::vector<int> h_neighbor_points_indices(neighbor_points_count);
     std::vector<float> h_normals(points_array.size() * 3);
@@ -450,11 +613,23 @@ extern void ComputeNormals(std::vector<std::vector<float>> points_array,std::vec
     std::vector<long long int> h_eigen_compute_time(points_array.size());
     // std::cout<<"3.02"<<std::endl;
     //デバイス1次配列宣言
+	//kd
+	float *d_test_points;
+	int *d_parent_ids,*d_left_ids,*d_right_ids,*d_axes;
+	//normal
     float *d_points,*d_normals,*d_curvatures;
     int *d_neighbor_points_indices,*d_neighbor_start_indices;
     long long int *d_covariance_compute_time,*d_eigen_compute_time;
+
+
     // std::cout<<"3.03"<<std::endl;
     //メモリ確保
+	cudaMalloc((void **)&d_test_points, points.size() * 3 * sizeof(float));
+	cudaMalloc((void **)&d_parent_ids, points.size() * sizeof(int));
+	cudaMalloc((void **)&d_left_ids, points.size() * sizeof(int));
+	cudaMalloc((void **)&d_right_ids, points.size() * sizeof(int));
+	cudaMalloc((void **)&d_axes, points.size() * sizeof(int));
+
     cudaMalloc((void **)&d_points, points_array.size() * 3 * sizeof(float));
     cudaMalloc((void **)&d_neighbor_points_indices, neighbor_points_count * sizeof(int));
     cudaMalloc((void **)&d_neighbor_start_indices, points_array.size() * sizeof(int));
@@ -475,8 +650,27 @@ extern void ComputeNormals(std::vector<std::vector<float>> points_array,std::vec
             l++;
         }
     }
+	k=0;
+	for(int i=0;i<points.size();i++){
+		for(int j=0;j<3;j++){
+			h_test_points[k]=points[i][j];
+			k++;
+		}
+		h_parent_ids[i]=nodes[i].parent_id;
+		h_left_ids[i]=nodes[i].left_id;
+		h_right_ids[i]=nodes[i].right_id;
+		h_axes[i]=nodes[i].axis;
+	}
+
+
     // std::cout<<"3.05"<<std::endl;
     //コピー
+	cudaMemcpy(d_test_points, &h_test_points[0], points.size() * 3 * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_parent_ids, &h_parent_ids[0], points.size() * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_left_ids, &h_left_ids[0], points.size() * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_right_ids, &h_right_ids[0], points.size() * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_axes, &h_axes[0], points.size() * sizeof(int), cudaMemcpyHostToDevice);
+
     cudaMemcpy(d_points, &h_points[0], points_array.size() * 3 * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_neighbor_points_indices, &h_neighbor_points_indices[0], neighbor_points_count * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_neighbor_start_indices, &neighbor_start_indices[0], points_array.size() * sizeof(int), cudaMemcpyHostToDevice);
@@ -489,7 +683,7 @@ extern void ComputeNormals(std::vector<std::vector<float>> points_array,std::vec
     // std::cout<<"normalsGPUstart"<<std::endl;
     cudaDeviceSetLimit(cudaLimitStackSize, 1024*8);
     //実行
-    NormalsGPU<<<grid,block>>>(d_points,points_array.size(),d_neighbor_points_indices,d_neighbor_start_indices,neighbor_points_count,d_normals,d_curvatures,d_covariance_compute_time,d_eigen_compute_time);
+    NormalsGPU<<<grid,block>>>(d_test_points,d_parent_ids,d_left_ids,d_right_ids,d_axes,root_id,points.size(),d_points,points_array.size(),d_neighbor_points_indices,d_neighbor_start_indices,neighbor_points_count,d_normals,d_curvatures,d_covariance_compute_time,d_eigen_compute_time);
     // std::cout<<"normalsGPUend"<<std::endl;
     // std::cout<<"3.08"<<std::endl;
     //コピー
@@ -512,6 +706,13 @@ extern void ComputeNormals(std::vector<std::vector<float>> points_array,std::vec
     // std::cout<<"cu_normals : "<<normals_array[0][0]<<","<<normals_array[0][1]<<","<<normals_array[0][2]<<std::endl;
     // std::cout<<"3.10"<<std::endl;
     //メモリ解放
+	//kd
+	cudaFree(d_test_points);
+	cudaFree(d_parent_ids);
+	cudaFree(d_left_ids);
+	cudaFree(d_right_ids);
+	cudaFree(d_axes);
+	//normal
     cudaFree(d_points);
     cudaFree(d_neighbor_points_indices);
     cudaFree(d_neighbor_start_indices);
